b3715f44ed2ca191b3410d6890edaa815e091bfb
yakzaetz <yakzaetz@gmail.com>
2021-12-30 15:03:54 -0500

    completed phase 2 of sub_sum

--- Execution_Time_Difference.rb
+++ Execution_Time_Difference.rb
@@ -37,29 +37,43 @@ require "byebug"
 # list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # p my_min(list)  # =>  -5
 
-#n^2 + n => n^2
-def sub_sum(arr)
-    new_arr = []
-    (0...arr.length).each do |i|
-        (i...arr.length).each do |i2|
-            new_arr << arr[i..i2]
-        end
-    end 
+# #n^2 + n => n^2
+# def sub_sum(arr)
+#     new_arr = []
+#     (0...arr.length).each do |i|
+#         (i...arr.length).each do |i2|
+#             new_arr << arr[i..i2]
+#         end
+#     end 
    
-    max = new_arr[0]
-    new_arr.each do |sub|
-        max = sub if sub.sum > max.sum
-    end
-    max.sum
-end
-list = [5, 3, -7]
-p sub_sum(list) # => 8
+#     max = new_arr[0]
+#     new_arr.each do |sub|
+#         max = sub if sub.sum > max.sum
+#     end
+#     max.sum
+# end
+# list = [5, 3, -7]
+# p sub_sum(list) # => 8
 
 #no sub_sets to get the answer
 #keep track of the largest sum vs cur_sum
 # we can have only one iteration and 
 # check max_sum and cur_sum, from cur_pos + nex_pos = cur_sum check which max_sum or cur_sum is greated, if cur is greater. reassign
-# break the while loop if list.length   
-def sub_sum()
+# break the while loop if list.length  
+#time complexity O(n) 
+def sub_sum(arr)
+    current_sum = arr.first #current_sum = 5
+    max_sum = current_sum # 5
 
+    (1...arr.length).each do |i| #arr[i] = -6
+        current_sum += arr[i] #current_sum = -1
+        current_sum = 0 if current_sum < 0 
+        if current_sum > max_sum
+            max_sum = current_sum
+        end 
+    end 
+    max_sum
 end
+
+list = [2, 3, -6, 7, -6, 7]
+p sub_sum(list) # => 8 (from [7, -6, 7])
