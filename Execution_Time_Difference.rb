571f8e06e569e07e31218ec4710ae1159c6fbacf
yakzaetz <yakzaetz@gmail.com>
2021-12-30 14:11:41 -0500

    completed my_min and started sub_sum

--- Execution_Time_Difference.rb
+++ Execution_Time_Difference.rb
@@ -1,19 +1,42 @@
 #nested 
 require "byebug"
-def my_min(arr)
-    min = 0
-    (0...arr.length).each do |i|
-        (i+1...arr.length).each do |i2|
-            debugger
-            if arr[i] < arr[i2]
-                min = arr[i]
-            else
-                min = arr[i2]
-            end
-        end
-    end
-    min
-end
+#time complexity O(n^2)
+# def my_min(arr)
+#     min = 0
+#     (0...arr.length).each do |i|
+#         new_arr = arr[i+1..-1]
+#         new_arr.any? do |ele| 
+#             if ele < min 
+#                 min = ele
+#             end
+#         end 
+#     end 
 
-list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
-p my_min(list)  # =>  -5
+#         # (i+1...arr.length).each do |i2|
+#         #     # debugger
+#         #     if arr[i] < arr[i2]
+#         #         min = arr[i]
+#         #     else
+#         #         min = arr[i2]
+#         #     end
+#         # end
+#     min
+# end
+
+#time complexity O(n)
+# def my_min(arr)
+#     arr.inject do |acc, ele| 
+#         if acc < ele 
+#             acc
+#         else
+#             ele 
+#         end 
+#     end 
+# end
+
+# list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
+# p my_min(list)  # =>  -5
+
+def sub_sum(matrix)
+    
+end
