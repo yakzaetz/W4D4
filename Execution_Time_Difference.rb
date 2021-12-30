f8ff2fc949a6235fe24aca58dfebd7c9fc2437f6
Katie Hang <katiehang1991@gmail.com>
2021-12-30 15:28:04 -0500

    wrap up project execution time difference

--- Execution_Time_Difference.rb
+++ Execution_Time_Difference.rb
@@ -1,59 +1,60 @@
 #nested 
 require "byebug"
-#time complexity O(n^2)
-# def my_min(arr)
-#     min = 0
-#     (0...arr.length).each do |i|
-#         new_arr = arr[i+1..-1]
-#         new_arr.any? do |ele| 
-#             if ele < min 
-#                 min = ele
-#             end
-#         end 
-#     end 
+time complexity O(n^3)
+space complecity O(n^2)
+def my_min(arr)
+    min = 0
+    (0...arr.length).each do |i| #n 
+        new_arr = arr[i+1..-1] #m O(n)  
+        new_arr.any? do |ele| #m 
+            if ele < min 
+                min = ele
+            end
+        end 
+    end 
 
-#         # (i+1...arr.length).each do |i2|
-#         #     # debugger
-#         #     if arr[i] < arr[i2]
-#         #         min = arr[i]
-#         #     else
-#         #         min = arr[i2]
-#         #     end
-#         # end
-#     min
-# end
+        # (i+1...arr.length).each do |i2|
+        #     # debugger
+        #     if arr[i] < arr[i2]
+        #         min = arr[i]
+        #     else
+        #         min = arr[i2]
+        #     end
+        # end
+    min
+end
 
-#time complexity O(n)
-# def my_min(arr)
-#     arr.inject do |acc, ele| 
-#         if acc < ele 
-#             acc
-#         else
-#             ele 
-#         end 
-#     end 
-# end
+time complexity O(n)
+def my_min(arr)
+    arr.inject do |acc, ele| 
+        if acc < ele 
+            acc
+        else
+            ele 
+        end 
+    end 
+end
 
 # list = [ 0, 3, 5, 4, -5, 10, 1, 90 ]
 # p my_min(list)  # =>  -5
 
 # #n^2 + n => n^2
-# def sub_sum(arr)
-#     new_arr = []
-#     (0...arr.length).each do |i|
-#         (i...arr.length).each do |i2|
-#             new_arr << arr[i..i2]
-#         end
-#     end 
+def sub_sum(arr)
+    new_arr = []
+    (0...arr.length).each do |i|
+        (i...arr.length).each do |i2|
+            new_arr << arr[i..i2]
+        end
+    end 
    
-#     max = new_arr[0]
-#     new_arr.each do |sub|
-#         max = sub if sub.sum > max.sum
-#     end
-#     max.sum
-# end
-# list = [5, 3, -7]
-# p sub_sum(list) # => 8
+    max = new_arr[0]
+    new_arr.each do |sub|
+        max = sub if sub.sum > max.sum
+    end
+    max.sum
+end
+list = [5, 3, -7]
+p sub_sum(list) # => 8
 
 #no sub_sets to get the answer
 #keep track of the largest sum vs cur_sum
